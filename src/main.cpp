69381c2cd6f7e4fb426d8e5ba16626f19443af8a
mak <makushevski@gmail.com>
2025-05-24 18:20:16 +0500

    add libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,20 @@
 ﻿#include <Arduino.h>
-int trigPin = 12;
-int echoPin = 13;
+#include <IRremote.h>
 
-int duration, cm, inches;
+int RECV_PIN = 3;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 
 void setup() {
     Serial.begin(9600);
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
+    irrecv.enableIRIn();
 }
 
 void loop() {
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
+    if (irrecv.decode(&results)) {
+        Serial.println(results.value, HEX);
+        irrecv.resume();
+    }
 
-    duration = pulseIn(echoPin, HIGH);
-    cm = (duration / 2) / 29.1;
-    inches = (duration / 2) / 74;
-
-    Serial.print(inches);
-    Serial.print(" inches, ");
-    Serial.print(cm);
-    Serial.print(" cm");
-    Serial.println();
+    delay(100);
 }
