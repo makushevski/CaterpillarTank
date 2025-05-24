6ad359a17bbb3c315abf3caf5935511900aedc42
mak <makushevski@gmail.com>
2025-05-24 17:11:46 +0500

    add the first initialisation

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <Arduino.h>
+int trigPin = 12;
+int echoPin = 13;
+
+int duration, cm, inches;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+}
+
+void loop() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    duration = pulseIn(echoPin, HIGH);
+    cm = (duration / 2) / 29.1;
+    inches = (duration / 2) / 74;
+
+    Serial.print(inches);
+    Serial.print(" inches, ");
+    Serial.print(cm);
+    Serial.print(" cm");
+    Serial.println();
+}
---
+++ src/modules/ultrasonicSensor.h
@@ -0,0 +1,8 @@
+﻿//
+// Created by Denis on 24.05.2025.
+//
+
+#ifndef ULTRASONICSENSOR_H
+#define ULTRASONICSENSOR_H
+
+#endif //ULTRASONICSENSOR_H
